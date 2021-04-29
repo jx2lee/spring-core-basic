5c8c4aa4faba959d4e697beeeca28dbf52283fcd
jx2lee <jaejun.lee.1991@gmail.com>
2021-04-30 00:12:55 +0900

    from AppConfig 리팩터링

--- src/main/java/com/jx2lee/core/AppConfig.java
+++ src/main/java/com/jx2lee/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.jx2lee.core;
 
+import com.jx2lee.core.discount.DiscountPolicy;
 import com.jx2lee.core.discount.FixDiscountPolicy;
+import com.jx2lee.core.member.MemberRepository;
 import com.jx2lee.core.member.MemberService;
 import com.jx2lee.core.member.MemberServiceImpl;
 import com.jx2lee.core.member.MemoryMemberRepository;
@@ -10,11 +12,18 @@ import com.jx2lee.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    // Memory DB 말고 다른 DB 를 사용할 때는 이 부분만 수정하면 된다.
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
-
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    // 할인 정책이 바뀐 경우 아래 return 부분만 추가하여 수정하면 된다.
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
