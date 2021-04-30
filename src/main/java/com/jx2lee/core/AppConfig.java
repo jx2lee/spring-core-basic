34d3b9881d04722d4ee02d3a5f5930e763dc2733
jx2lee <jaejun.lee.1991@gmail.com>
2021-04-30 11:46:04 +0900

    from 새로운 구조와 할인 정책 적용

--- src/main/java/com/jx2lee/core/AppConfig.java
+++ src/main/java/com/jx2lee/core/AppConfig.java
@@ -2,6 +2,7 @@ package com.jx2lee.core;
 
 import com.jx2lee.core.discount.DiscountPolicy;
 import com.jx2lee.core.discount.FixDiscountPolicy;
+import com.jx2lee.core.discount.RateDiscountPolicy;
 import com.jx2lee.core.member.MemberRepository;
 import com.jx2lee.core.member.MemberService;
 import com.jx2lee.core.member.MemberServiceImpl;
@@ -14,6 +15,7 @@ public class AppConfig {
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+
     // Memory DB 말고 다른 DB 를 사용할 때는 이 부분만 수정하면 된다.
     private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
@@ -22,8 +24,13 @@ public class AppConfig {
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    // 할인 정책이 바뀐 경우 아래 return 부분만 추가하여 수정하면 된다.
+
+    /**
+     * 할인 정책
+     * 할인 정책이 바뀐 경우 아래 return 부분만 추가하여 수정하면 된다.
+     */
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
