2e653e242aec9dce7b5ea484d19cd5607dc50340
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-04 16:45:47 +0900

    from 스프링으로 전환하기

--- src/main/java/com/jx2lee/core/AppConfig.java
+++ src/main/java/com/jx2lee/core/AppConfig.java
@@ -1,7 +1,6 @@
 package com.jx2lee.core;
 
 import com.jx2lee.core.discount.DiscountPolicy;
-import com.jx2lee.core.discount.FixDiscountPolicy;
 import com.jx2lee.core.discount.RateDiscountPolicy;
 import com.jx2lee.core.member.MemberRepository;
 import com.jx2lee.core.member.MemberService;
@@ -9,18 +8,24 @@ import com.jx2lee.core.member.MemberServiceImpl;
 import com.jx2lee.core.member.MemoryMemberRepository;
 import com.jx2lee.core.order.OrderService;
 import com.jx2lee.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // Memory DB 말고 다른 DB 를 사용할 때는 이 부분만 수정하면 된다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -29,6 +34,7 @@ public class AppConfig {
      * 할인 정책
      * 할인 정책이 바뀐 경우 아래 return 부분만 추가하여 수정하면 된다.
      */
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/com/jx2lee/core/OrderApp.java
+++ src/main/java/com/jx2lee/core/OrderApp.java
@@ -5,16 +5,22 @@ import com.jx2lee.core.member.Member;
 import com.jx2lee.core.member.MemberService;
 import com.jx2lee.core.order.Order;
 import com.jx2lee.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 //        MemberServiceImpl memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
 
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
