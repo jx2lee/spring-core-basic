2df52cd0ff72ef5bf2c5fb7db34d4ab00d5a8b83
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-07 09:20:24 +0900

    from 스프링 빈 조회 - 상속 관계

--- src/test/java/com/jx2lee/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/com/jx2lee/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ApplicationContextSameBeanFindTest {
+class ApplicationContextSameBeanFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
