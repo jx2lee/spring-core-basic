9278fe6a20e99af3386a20d9328852f25b0a5e97
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-10 14:45:39 +0900

    from 싱글톤 패턴

---
+++ src/test/java/com/jx2lee/core/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package com.jx2lee.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("singleton 객체 로직 호출");
+    }
+}
--- src/test/java/com/jx2lee/core/singleton/SingletonTest.java
+++ src/test/java/com/jx2lee/core/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package com.jx2lee.core.singleton;
 
 import com.jx2lee.core.AppConfig;
 import com.jx2lee.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SingletonTest {
 
@@ -23,6 +24,20 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("Singleton 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same == 참조값 비교
+        //equal == 참조값 비교 X
     }
 }
