e1e6ef409b9ee01e562a52a3a9757e071483c9d2
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-11 11:29:55 +0900

    from @Configuration과 싱글톤

--- src/main/java/com/jx2lee/core/AppConfig.java
+++ src/main/java/com/jx2lee/core/AppConfig.java
@@ -14,19 +14,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository
+    // @Bean orderService -> new MemoryMemberRepository
+
+    // memberRepository 함수 호출 확인 (코드상)
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // memberRepository 함수 호출 확인 (실제)
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     // Memory DB 말고 다른 DB 를 사용할 때는 이 부분만 수정하면 된다.
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
