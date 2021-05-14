2b2252b2b36fa02df99ed6f68c2e0496f9e02e5f
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-14 14:12:59 +0900

    from 옵션 처리

---
+++ src/test/java/com/jx2lee/core/autowired/AutowiredTest.java
@@ -0,0 +1,36 @@
+package com.jx2lee.core.autowired;
+
+import com.jx2lee.core.member.Member;
+import com.sun.istack.internal.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
+    }
+
+    static class TestBean {
+        @Autowired(required = false)
+        public void noSetBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void noSetBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void noSetBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
