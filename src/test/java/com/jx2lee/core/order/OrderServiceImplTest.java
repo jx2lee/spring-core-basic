b3e269f50117a10cd159e99322171ebd44fe2857
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-14 15:34:31 +0900

    from 생성자 주입을 선택해라!

---
+++ src/test/java/com/jx2lee/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.jx2lee.core.order;
+
+import com.jx2lee.core.discount.FixDiscountPolicy;
+import com.jx2lee.core.member.Grade;
+import com.jx2lee.core.member.Member;
+import com.jx2lee.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        memoryMemberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
