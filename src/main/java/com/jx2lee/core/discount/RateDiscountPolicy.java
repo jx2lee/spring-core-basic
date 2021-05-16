3288e9321f8ce7fc2a3e9a3ffad9fccfb846574e
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-16 23:13:23 +0900

    from 애노테이션 직접 생성

--- src/main/java/com/jx2lee/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/jx2lee/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package com.jx2lee.core.discount;
 
+import com.jx2lee.core.annotation.MainDiscountPolicy;
 import com.jx2lee.core.member.Grade;
 import com.jx2lee.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/com/jx2lee/core/order/OrderServiceImpl.java
+++ src/main/java/com/jx2lee/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jx2lee.core.order;
 
+import com.jx2lee.core.annotation.MainDiscountPolicy;
 import com.jx2lee.core.discount.DiscountPolicy;
 import com.jx2lee.core.member.Member;
 import com.jx2lee.core.member.MemberRepository;
@@ -14,14 +15,12 @@ public class OrderServiceImpl implements OrderService{
 
     // 자동으로 의존 관계를 주입하는 Autowired
     // 주입을 자동으로 주입하길 원하는 생성자에 해당 annotation 작성
+    // RequiredArgsConstructor annotion 사용 시 아래 생성자를 자동으로 만들어준다.
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-    // RequiredArgsConstructor annotion 사용 시 아래 생성자를 자동으로 만들어준다.
-    // 대..박
-
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
