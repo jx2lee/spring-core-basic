1d8939b2e160b033c15e2291d45f40113e6b381d
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-17 23:08:50 +0900

    from 인터페이스 InitializingBean, DisposableBean

--- src/test/java/lifecycle/NetWorkClient.java
+++ src/test/java/lifecycle/NetWorkClient.java
@@ -1,13 +1,14 @@
 package lifecycle;
 
-public class NetWorkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetWorkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetWorkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,18 @@ public class NetWorkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetWorkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetWorkClient.destroy");
+        disconnect();
     }
 }
