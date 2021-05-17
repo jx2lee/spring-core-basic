391fb56622ca0821695a0a26ff00b868a4453904
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-17 22:56:47 +0900

    from 빈 생명주기 콜백 시작

---
+++ src/test/java/lifecycle/NetWorkClient.java
@@ -0,0 +1,30 @@
+package lifecycle;
+
+public class NetWorkClient {
+
+    private String url;
+
+    public NetWorkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("cal: " + url + "message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
